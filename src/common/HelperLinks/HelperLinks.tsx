f12bb3b85c8354e68c38567b118c792f8d90a0a7
nossbigg <chenggibson@gmail.com>
2021-09-01 01:01:39 +1000

    Add /user-cached-ssp page to HelperLinks

--- src/common/HelperLinks/HelperLinks.tsx
+++ src/common/HelperLinks/HelperLinks.tsx
@@ -1,9 +1,16 @@
+// not using nextjs <Link /> to deliberately avoid prefetching
+/* eslint-disable @next/next/no-html-link-for-pages */
+
 export const HelperLinks = () => {
   return (
     <div>
       <a href="/user">/user page</a>
       <br />
-      <a href="/user-cached">/user-cache page</a>
+      <a href="/user-cached-ip">/user-cached-ip page (getInitialProps())</a>
+      <br />
+      <a href="/user-cached-ssp">
+        /user-cached-ssp page (getServerSideProps())
+      </a>
       <br />
       <button onClick={onClearCacheButton}>Clear User Data Cache</button>
     </div>
